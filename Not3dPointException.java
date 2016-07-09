ec2548e76672d34ee1444a30058f0154a642f750
Tyler <shed412@gmail.com>
2016-07-09 19:51:42 -0400

    First commit. Finished Point constructor

---
+++ Not3dPointException.java
@@ -0,0 +1,7 @@
+public class Not3DPointException extends Exception
+{
+  public Not3DPointException(){ super(); }
+  public Not3DPointException(String mess){ super(mess); }
+  public Not3DPointException(String mess, Throwable cause){ super(mess, cause); }
+  public Not3DPointException(Throwable cause){ super(cause); }
+}//Exceoption
---
+++ Point.java
@@ -0,0 +1,32 @@
+public class Point
+{
+  //Coordinates for Point3
+  double[] coor;
+  boolean init = false;
+
+  public Point()
+  {
+    this.coor = new double[3];
+    init = true;
+  }//empty constructor
+
+  public Point(double x, double y, double z)
+  {
+    this.coor = new double[3];
+    this.coor[0] = x;
+    this.coor[1] = y;
+    this.coor[2] = z;
+    init = true;
+  }//x,y,z constructor
+
+  public Point(double[] coor) throws Not3DPointException
+  {
+      if(coor.length!=3){
+        init = false;
+        throw new Not3DPointException("Needs 3 points in the Array");
+      }else{
+        this.coor = coor;
+        init = true;
+      }//if else
+  }//construc
+}//coordinate constructor
