a46b113fd46a28f63bd7e44139c94309353196c7
Tyler <shed412@gmail.com>
2016-07-09 22:32:19 -0400

    Working on the JFrame

--- Test.java
+++ Test.java
@@ -1,27 +1,27 @@
-public class Test
+import javax.swing.*;
+import java.awt.*;
+
+public class Test extends JFrame
 {
-  public static void main(String[] args) throws NotInitializedException, Not3DPointException
-   {
-      double[] pa1 = {1,2,1};
-      double[] pa2 = {0,4,4};
-      Point p1 = new Point(pa1);
-      Point p2 = new Point(pa2);
-      Vector v1 = new Vector(2,0,0);
-      Vector v2;
+  public Test()
+  {
+    super("Game Frame");
+    setContentPane(new DrawPanel());
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(600, 600);
+    setVisible(true);
+  }//frame constructor
 
-      p1.drawPoint();
-      p2.drawPoint();
-
-      v2 = p1.subtractPointFromPoint(p2);
-      System.out.println(v2.toString());
-      System.out.println(v1.toString());
-      v1 = v1.addVectorToVector(v2);
-      System.out.println(v1.toString());
-
-      p1.addVectorToPoint(v1);
-      p1.drawPoint();
-
-      p2.subtractVectorFromPoint(v2);
-      p2.drawPoint();
+  class DrawPanel extends JPanel
+  {
+    public void paintComponent(Graphics g){
+          //draw on g here e.g.
+           g.fillRect(20, 20, 100, 200);
+         }
   }
+
+  public static void main(String[] args) throws NotInitializedException, Not3DPointException
+  {
+      new Test();
+  }//main
 }//test
