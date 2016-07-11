2fcdbdfc714d5436990a7128dcae39342df2b131
Tyler <shed412@gmail.com>
2016-07-11 15:45:17 -0400

    Added LinearTransformation class.

---
+++ LinearTransformation.java
@@ -0,0 +1,12 @@
+
+public class LinearTransformation
+{
+    private int n;
+    private double[][] transformation;
+
+    public LinearTransformation(int n)
+    {
+        this.n = n;
+        transformation = new double[n][n];
+    }//constructor
+}//LinearTransformation
