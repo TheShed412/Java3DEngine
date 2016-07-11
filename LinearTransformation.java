e892ecffecc74940f4093276e060216a447899bd
Tyler <shed412@gmail.com>
2016-07-11 18:41:41 -0400

    Did some things in LinearTrans

--- LinearTransformation.java
+++ LinearTransformation.java
@@ -9,4 +9,36 @@ public class LinearTransformation
         this.n = n;
         transformation = new double[n][n];
     }//constructor
+
+    public LinearTransformation(int n, double[][] tranArr)
+    {
+        this.n = n;
+        transformation = new double[n][n];
+        makeTransformation(tranArr);
+    }//constructor
+
+    public double[][] makeTransformation(double[][] tranArr)
+    {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                transformation[i][j] = tranArr[i][j];
+            }//inner for
+        }//outer for
+        return transformation;
+    }//makeTransformation
+
+    public String toString()
+    {
+        String returnString = "";
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(transformation[i][j]);
+            }//inner for
+            System.out.println();
+        }//outer for
+
+        return returnString;
+    }//toString
+
 }//LinearTransformation
--- Test.java
+++ Test.java
@@ -8,6 +8,10 @@ public class Test
 {
   public static void main(String[] args) throws NotInitializedException, Not3DPointException
   {
-      new GameFrame();
+      double[][] lt = {{1,1,1},{1,1,1}, {1,1,1}};
+
+      LinearTransformation l = new LinearTransformation(3, lt);
+
+      System.out.print(l);
   }//main
 }//test
