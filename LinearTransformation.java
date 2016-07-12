5ea3eb643b4b1643e2ac83e1fd5d073907593862
Tyler <shed412@gmail.com>
2016-07-11 20:01:50 -0400

    Finished the linear multiplication process

--- LinearTransformation.java
+++ LinearTransformation.java
@@ -27,6 +27,51 @@ public class LinearTransformation
         return transformation;
     }//makeTransformation
 
+    public double[][] getMatrix()
+    {
+        double[][] returnArr = new double[5][5];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                returnArr[i][j] = transformation[i][j];
+            }//inner for
+        }//outer for
+
+        return returnArr;
+    }//getMatrix
+
+    public int getSize()
+    {
+        return n;
+    }//getSize
+
+    public LinearTransformation matrixMultiply(LinearTransformation mult)
+    {
+        double[][] multArr = mult.getMatrix();
+        double[][] resultArr = new double[n][n];
+        LinearTransformation result = new LinearTransformation(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                resultArr[i][j] = multiply(multArr, j, i);
+            }//inner for
+        }//outer for
+
+        result.makeTransformation(resultArr);
+
+        return result;
+    }//matrixMultiply
+
+    private double multiply(double[][] mult, int col, int row)
+    {
+        double result = 0.0;
+
+        for(int i=0; i<n; i++){
+            result += transformation[row][i]*mult[i][col];
+        }//for loop
+
+        return result;
+    }//multiply
+
     public String toString()
     {
         String returnString = "";
--- Test.java
+++ Test.java
@@ -8,10 +8,14 @@ public class Test
 {
   public static void main(String[] args) throws NotInitializedException, Not3DPointException
   {
-      double[][] lt = {{1,1,1},{1,1,1}, {1,1,1}};
+      double[][] lt = {{2,7,4},{5,4,2}, {7,4,8}};
+      double[][] lt2 = {{8,5,4},{5,4,2}, {7,4,8}};
 
       LinearTransformation l = new LinearTransformation(3, lt);
+      LinearTransformation l2 = new LinearTransformation(3, lt2);
 
-      System.out.print(l);
+      LinearTransformation l3 = l.matrixMultiply(l2);
+
+      System.out.print(l3);
   }//main
 }//test
