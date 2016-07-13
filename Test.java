17fbbfab4e4864cb5d61f003738223cd33f5320a
Tyler <shed412@gmail.com>
2016-07-13 19:07:34 -0400

    Weird thing happened with the Test file

--- Test.java
+++ Test.java
@@ -8,14 +8,6 @@ public class Test
 {
   public static void main(String[] args) throws NotInitializedException, Not3DPointException
   {
-      double[][] lt = {{2,7,4},{5,4,2}, {7,4,8}};
-      double[][] lt2 = {{8,5,4},{5,4,2}, {7,4,8}};
-
-      LinearTransformation l = new LinearTransformation(3, lt);
-      LinearTransformation l2 = new LinearTransformation(3, lt2);
-
-      LinearTransformation l3 = l.matrixMultiply(l2);
-
-      System.out.print(l3);
+    new GameFrame();
   }//main
 }//test
